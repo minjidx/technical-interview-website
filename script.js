c68dd34a23847910f59ecbc6b563b0692f6c7ce4
Minji <alswldx@gmail.com>
2026-09-05 17:26:21 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+const checkButton = document.getElementById("check-button");
+const result = document.getElementById("result");
+
+checkButton.addEventListener("click", function () {
+    result.classList.remove("hidden");
+});
